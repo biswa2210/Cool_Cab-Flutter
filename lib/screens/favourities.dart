65c25292889b955299605afa3bcf9148a0a4ca64
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-07-04 20:15:16 +0530

    BY BISWA

--- lib/screens/favourities.dart
+++ lib/screens/favourities.dart
@@ -1,4 +1,9 @@
 import 'package:cool_cab/database/Car.dart';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 import 'package:cool_cab/screens/car_details.dart';
 import 'package:cool_cab/widgets/BrandDevidder.dart';
 import 'package:firebase_auth/firebase_auth.dart';

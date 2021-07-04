43370053ca3720cfb4a19a001a8079fd9f7268b4
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-07-04 20:12:42 +0530

    BY BISWA

--- lib/screens/ComparisonSc.dart
+++ lib/screens/ComparisonSc.dart
@@ -1,4 +1,9 @@
 import 'package:cool_cab/database/Car.dart';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 import 'package:cool_cab/dataprovider/appdata.dart';
 import 'package:cool_cab/widgets/ProgressBar.dart';
 import 'package:firebase_auth/firebase_auth.dart';

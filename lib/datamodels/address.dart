6e564fe38dd3cac9c7d0631b8199d8016abbe95a
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-07-04 20:05:09 +0530

    BY BISWA

--- lib/datamodels/address.dart
+++ lib/datamodels/address.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 class AddressDatatype{
 String placeName;
 double latitude;

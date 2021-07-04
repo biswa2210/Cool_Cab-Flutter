2e165ac75192b35ada48171d468453313553ac6f
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-07-04 20:10:08 +0530

    BY BISWA

--- lib/dataprovider/appdata.dart
+++ lib/dataprovider/appdata.dart
@@ -1,6 +1,11 @@
 import 'package:cool_cab/datamodels/address.dart';
 import 'package:flutter/material.dart';
 class AppData extends ChangeNotifier{
+  /*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
   AddressDatatype pickAddress;
   void updatePickupAddress(AddressDatatype pickup){
     pickAddress = pickup;
